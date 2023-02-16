1fbe340a6bbb5962805a187734164406793a48b1
EC2 Default User <ec2-user@ip-172-31-5-87.ap-northeast-1.compute.internal>
2023-02-16 10:16:52 +0000

    [修正]devise機能の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_11_143414) do
+ActiveRecord::Schema.define(version: 2023_02_16_091655) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2023_02_11_143414) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_deleted"
+    t.boolean "is_deleted", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
